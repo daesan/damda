0e11e0bfc449dd0eeaa2939a83d71e10c02ee766
daesan <daesan@arabica.local>
2015-12-14 15:33:56 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# damda
-Damda is a state/actions container for Javascript client apps
+# Damda
+
+Damda is a state/actions container for Javascript client apps.
+
+It simplifies react and react native app development by separating data management from view rendering codes.
+
+Damda was inspired by Redux. While the core ideas of Redux are inherited, Damda differs from Redux in that actions and reducer functions are not separate entities. In Damda, actions are state change functions that can be dispatched directly.
+
+
+```js
+const { createStore, createActionGroup } = require('damda')
+
+/**
+ * This is action group definition.
+ * Each action is a pure function with (state) => state signature.
+ * 
+ * The shape of the state can be a nested javascript object.
+ */
+const counter = createActionGroup({
+  default: () => 0,
+  increment: (state) => state+1,
+  decrement: (state) => state-1
+})
+
+let store = createStore(counter)
+
+// You can subscribe to state changes.
+store.subscribe((state) => console.log(state))
+
+// You can change internal state by dispatching actions.
+store.dispatch(counter.increment)
+// 1
+store.dispatch(counter.increment)
+// 2
+store.dispatch(counter.decrement)
+// 1
+```
---
+++ src/index.js
@@ -0,0 +1,112 @@
+const createStore = function(actions) {
+  let store = {
+    state: null,
+    previousState: null,
+    processor: createProcessor(actions),
+    listeners: [],
+    dispatch: function(action, params) {
+      console.log('Action: ' + (action ? (action.prototype.name+(action.prototype.simple ? '' : '(Complex)' )) : 'Store Initialization'))
+
+      if (action === null) {
+        this.previousState = this.state
+        this.state = this.processor.process(null, action, params)
+        this.listeners.forEach((listener) => listener())
+        this.previousState = null
+      }
+      else if (action.prototype.simple===true) {
+        this.previousState = this.state
+        this.state = this.processor.process(this.state, action, params)
+        this.listeners.forEach((listener) => listener(this.state, this.previousState))
+        this.previousState = null
+      } else if (action.prototype.simple===false) {
+        action()(this, params)
+      }
+    },
+    subscribe: function(listener) {
+      this.listeners.push(listener)
+      return () => { this.listeners = this.listeners.filter((l) => l!==listener) }
+    }
+  }
+  
+  store.dispatch(null)
+
+  return store
+}
+
+const createProcessor = function(obj, paths=[]) {
+  if (obj.type=='actionGroup') { // if obj is actionGroup, create a processor out of it
+    Object.keys(obj).forEach(function(actionName) {
+      let action = obj[actionName]
+
+      action.prototype.name = (paths.length==0 ? actionName : paths.join('.')+'.'+actionName)
+      
+      action.prototype.simple = (typeof action({}, {}) === 'function') ? false : true
+    })
+    
+    return {
+      process: function(state, action, params) {
+        if (state === null) {
+          return this.actionGroup.default()
+        } else {
+          for (let i=0; i<Object.keys(this.actionGroup).length; i++) {
+            let k = Object.keys(this.actionGroup)[i]
+  
+            if (this.actionGroup[k]===action) {
+              return this.actionGroup[k](state, params)
+            }
+          }
+                  
+          return state
+        }
+      },
+      actionGroup: obj
+    }
+  } else { // if obj is a normal object, combine processors in it
+    let subProcessors = {}
+    
+    Object.keys(obj).forEach((k) => {
+      subProcessors[k] = createProcessor(obj[k], [...paths, k])
+    })
+    
+    return {
+      process: function(state, action, params) {
+        if (state === null) {
+          let newState = {}
+
+          Object.keys(this.subProcessors).forEach((k) => {
+            newState[k] = this.subProcessors[k].process(null)
+          })
+          
+          return newState
+        } else {
+          let newState = {}
+          
+          Object.keys(this.subProcessors).forEach((k) => {
+            newState[k] = this.subProcessors[k].process(state[k], action, params)
+          })
+          
+          return newState
+        }
+      },
+      subProcessors: subProcessors
+    }
+  }
+}
+
+const createActionGroup = function(obj) {
+  let propertiesObject = {}
+
+  Object.keys(obj).forEach(function(actionName) {
+    let action = obj[actionName]
+
+    propertiesObject[actionName] = {enumerable: true, value: action}
+  })
+  
+  propertiesObject.type = {enumerable: false, value: 'actionGroup'}
+  
+  return Object.create({}, propertiesObject)
+}
+
+module.exports.createStore = createStore;
+module.exports.createProcessor = createProcessor;
+module.exports.createActionGroup = createActionGroup;
